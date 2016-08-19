5b54c9a738ca29700680ba75dcda657884c9515a
choochoopiston <choochoopiston@gmail.com>
2016-08-19 08:49:35 +0000

    Add validation to Question modek and Make question_spec file

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  #タイトルと内容が記入されていればOK 
+  it "is valid with title, content, user_id" do
+    question = Question.new(title: "あああ", content: "いいい", user_id: 1)
+    expect(question).to be_valid
+  end
+  
+  #タイトルと内容が記入されていなければNG
+  it "is invalid without title, content" do
+    question = Question.new
+    expect(question).not_to be_valid
+  end
+  
+  #タイトが記入されていなければNG
+  it "is invalid without title" do
+    question = Question.new(content:"いいい", user_id: 1)
+    expect(question).not_to be_valid
+  end
+  
+  #内容が記入されていなければNG
+  it "is invalid without content" do
+    question = Question.new(title:"あああ", user_id: 1)
+    expect(question).not_to be_valid
+  end
+  
+  #ユーザIDが記入されていなければNG
+  it "is invalid without content" do
+    question = Question.new(title:"あああ", content:"いいい")
+    expect(question).not_to be_valid
+  end
+  
 end
