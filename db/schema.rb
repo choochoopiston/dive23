57df2b1c542906d056f96f8792064b29c794d23f
norahito <nobuto310@gmail.com>
2016-08-21 07:19:36 +0000

    generated Favorite model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821060841) do
+ActiveRecord::Schema.define(version: 20160821071254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.boolean  "deleted_flg", default: false, null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.integer  "user_id"
