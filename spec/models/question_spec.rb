252c51983fcf19caa5235549fa31226419c5cd1f
norahito <nobuto310@gmail.com>
2016-08-22 05:25:31 +0000

    add setting to use FactoryGirl methods without prefix

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,31 +5,31 @@ RSpec.describe Question, type: :model do
   describe 'Validation' do
     #タイトルと内容が記入されていればOK 
     it "is valid with title, content, user_id" do
-      question = FactoryGirl.build(:question, user_id: 1)
+      question = build(:question, user_id: 1)
       expect(question).to be_valid
     end
     
     #タイトルと内容が記入されていなければNG
     it "is invalid without title, content" do
-      question = FactoryGirl.build(:question, title: nil, content: nil, user_id: 1)
+      question = build(:question, title: nil, content: nil, user_id: 1)
       expect(question).not_to be_valid
     end
     
     #タイトが記入されていなければNG
     it "is invalid without title" do
-      question = FactoryGirl.build(:question, title: nil, user_id: 1)
+      question = build(:question, title: nil, user_id: 1)
       expect(question).not_to be_valid
     end
     
     #内容が記入されていなければNG
     it "is invalid without content" do
-      question = FactoryGirl.build(:question, content: nil, user_id: 1)
+      question = build(:question, content: nil, user_id: 1)
       expect(question).not_to be_valid
     end
     
     #ユーザIDが記入されていなければNG
     it "is invalid without content" do
-      question = FactoryGirl.build(:question)
+      question = build(:question)
       expect(question).not_to be_valid
     end
   end
