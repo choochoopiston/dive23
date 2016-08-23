dd01dfc175f4053ef9d5f199bb23bf236778823a
norahito <nobuto310@gmail.com>
2016-08-23 02:42:05 +0000

    set association between tags and tag_relations

--- spec/models/tag_relation_spec.rb
+++ spec/models/tag_relation_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe TagRelation, type: :model do
   describe 'Association' do
     it { should belong_to(:question) }
+    it { should belong_to(:tag) }
   end
 end
