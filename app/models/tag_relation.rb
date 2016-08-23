698db519d10691d60d57af88cdf5e731c938eb2d
norahito <nobuto310@gmail.com>
2016-08-23 03:37:27 +0000

    set association between tags and tag_relations

--- app/models/tag_relation.rb
+++ app/models/tag_relation.rb
@@ -1,3 +1,4 @@
 class TagRelation < ActiveRecord::Base
   belongs_to :question
+  belongs_to :tag
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Association' do
+    it { should have_many(:tag_relations) }
+  end
 end
