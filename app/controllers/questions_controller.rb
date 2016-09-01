952aaa2fa5394c9b8e890f73cc215f50a8a5714c
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-09-02 08:50:25 +0900

    Added authentication about login.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @questions = Question.all
