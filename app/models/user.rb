9f163ccbc93686f59f32f1519aad90e82a684ae6
choochoopiston <choochoopiston@gmail.com>
2016-09-01 04:43:22 +0000

    Add mount_uploader to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  mount_uploader :avatar, AvatarUploader
+         
   has_many :answers
   has_many :questions
   has_many :favorites
   has_many :votes
+  
 end
