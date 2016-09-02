177745e0685a5e1d8a3871bae63ed37d7d97a7bd
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-09-02 10:06:31 +0900

    Added authentication for edit and delete.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,6 +14,9 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    unless check_mine?
+      redirect_to :questions, notice: '編集権限がありません' #TODO m.kitamura メッセージ定義
+    end
   end
 
   def create
@@ -29,20 +32,28 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-      else
-        format.html { render :edit }
+    if check_mine?
+      respond_to do |format|
+        if @question.update(question_params)
+          format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
       end
+    else
+      redirect_to :questions, notice: '編集権限がありません' #TODO m.kitamura メッセージ定義
     end
   end
 
   def destroy
-    @question.deleted_flg = true
-    @question.save
-    respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+    if check_mine?
+      @question.deleted_flg = true
+      @question.save
+      respond_to do |format|
+        format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+      end
+    else
+      redirect_to :questions, notice: '編集権限がありません' #TODO m.kitamura メッセージ定義
     end
   end
 
@@ -53,5 +64,13 @@ class QuestionsController < ApplicationController
 
     def question_params
       params.require(:question).permit(:user_id, :title, :content, :photo, :favorite_counts, :posi_counts, :nega_counts, :deleted_flg)
+    end
+
+    def check_mine?
+      if @question.user.id == current_user.id
+        true
+      else
+        false
+      end
     end
 end
