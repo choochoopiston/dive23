8d3d432b035ef35c89e9f6d8a3b694cca80d1e92
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-09-02 12:33:50 +0900

    Added show action for skip_before_action.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @questions = Question.all
