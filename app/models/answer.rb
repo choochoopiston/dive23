bbf3914ebfa09f00723ad3f6117f056b41246a54
choochoopiston <choochoopiston@gmail.com>
2016-09-07 12:33:48 +0000

    Add validation to Answer Model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,8 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   has_many :votes
   
+  validates_presence_of :content, :user_id, :question_id
+  
   default_scope -> { where(deleted_flg: false) }
   
 end
