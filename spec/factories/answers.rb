2e85ac2e7e59ee0d72cbe82f11f429595d4e36f2
choochoopiston <choochoopiston@gmail.com>
2016-09-07 15:10:50 +0000

    Made factories/answers.rb and models/answers_spec.rb

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :answer do
-    
+    sequence(:question_id) {|n| n + 1} 
+    sequence(:user_id) {|n| n + 1}
+    content "MyText"
+    photo "MyString"
   end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,5 +5,33 @@ RSpec.describe Answer, type: :model do
     it { should belong_to(:question) }
     it { should belong_to(:user) }
     it { should have_many(:votes) }
-  end    
+  end   
+  
+  describe 'Validation' do
+    #内容が入力、質問IDとユーザIDが入っていればOK 
+    it "is valid with content, question_id, user_id" do
+      answer = build(:answer)
+      expect(answer).to be_valid
+    end
+    
+    
+    #内容が記入されていなければNG
+    it "is invalid without content" do
+      answer = build(:answer, content: nil)
+      expect(answer).not_to be_valid
+    end
+    
+    #質問IDが入っていなければNG
+    it "is invalid without question_id" do
+      answer = build(:answer, question_id: nil)
+      expect(answer).not_to be_valid
+    end
+    
+    #ユーザIDが記入されていなければNG
+    it "is invalid without user_id" do
+      answer = build(:answer, user_id: nil)
+      expect(answer).not_to be_valid
+    end
+  end
+  
 end
