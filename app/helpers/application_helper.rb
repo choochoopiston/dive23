5355be266f2759a23fd237b55ba19eb6b7c198ad
norahito <nobuto310@gmail.com>
2016-09-08 10:18:21 +0900

    mypage established

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def profile_img(user)
+    if user.avatar?
+      image_tag(user.avatar, alt: user.name)
+    else
+      image_tag('no_image.png', alt: user.name)
+    end
+  end
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+    include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -30,11 +30,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  process :resize_to_limit => [200, 200]
+  
+  # Create thumbnail version of uploaded file. ex) @user.avator.thumb.url
+  version :thumb do
+    process :resize_to_fit => [60, 60]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
