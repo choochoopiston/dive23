82570c73ce80985d83afee82132a43f95e3f5022
choochoopiston <choochoopiston@gmail.com>
2016-09-10 01:46:50 +0000

    Added routing of users

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :favorites, only: [:create, :destroy]
   end
   devise_for :users
+  resources :users, only: [:index] 
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
