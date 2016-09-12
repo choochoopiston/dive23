7f58098652ca1041a65c0964e5b43f0b4066febc
norahito <nobuto310@gmail.com>
2016-09-12 14:33:29 +0900

    changed to resize_to_fill

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,14 +5,14 @@ class AvatarUploader < BaseUploader
   process :resize_to_limit => [200, 200]
   
   version :small do
-    process :resize_to_fit => [32, 32]
+    process :resize_to_fill => [32, 32]
   end
 
   version :medium do
-    process :resize_to_fit => [48, 48]
+    process :resize_to_fill => [48, 48]
   end
 
   version :large do
-    process :resize_to_fit => [164, 164]
+    process :resize_to_fill => [164, 164]
   end
 end
