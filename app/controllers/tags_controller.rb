3dc83dc8a5eb216856b18de18fdc895cf312c8e9
norahito <nobuto310@gmail.com>
2016-09-23 01:45:09 +0900

    tag index view and controller implemented

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,12 @@
 class TagsController < ApplicationController
   def index
+    case params[:tab]
+    when "name"
+      @tags = Tag.all.order(name: :asc)
+    when "new"
+      @tags = Tag.all.order(created_at: :desc)
+    else
+      @tags = Tag.all.order(name: :asc)
+    end
   end
 end
