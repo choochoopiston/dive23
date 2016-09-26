f75b074cc2a78efe97bb901d8a986500be166e3d
norahito <nobuto310@gmail.com>
2016-09-26 12:16:52 +0900

    fixed for review

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,10 @@
 class TagsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
-    case params[:tab]
-    when "name"
+    if params[:tab] == "name"
       @tags = Tag.all.order(name: :asc)
-    when "new"
-      @tags = Tag.all.order(created_at: :desc)
     else
-      @tags = Tag.all.order(name: :asc)
+      @tags = Tag.all.order(created_at: :desc)
     end
   end
 end
