d37429f2e89fe1ae409fbce9b0ee540fcbc7b9c0
Nobuto Uchiyama <nora@kundalini.local>
2016-10-10 18:26:54 +0900

    changed hard-delete to soft-delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
   mount_uploader :avatar, AvatarUploader
-         
+
   has_many :answers
   has_many :questions
   has_many :favorites
   has_many :votes
 
   validates_presence_of :name
-  
-  default_scope -> { where(deleted_flg: false) }
-  
+
   def favorite?(question)
     favorites.find_by(question_id: question.id)
   end
-  
+
   def vote_plus?(question)
     votes.where(is_positive: true).find_by(question_id: question.id)
   end
@@ -30,9 +28,14 @@ class User < ActiveRecord::Base
   def vote_minus?(question)
     votes.where(is_positive: false).find_by(question_id: question.id)
   end
-  
+
   def vote_minus_answer?(answer)
     votes.where(is_positive: false).find_by(answer_id: answer.id)
   end
-  
+
+  # prohibit deleted_user to sign in
+  def self.find_for_authentication(conditions)
+    super(conditions.merge(:deleted_flg => "false"))
+  end
+
 end
